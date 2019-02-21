25a75ebfc36cdd578925f2cc67b6bddf6bcf3505
Co-Tu Dpan <doan.cotu@gmail.com>
2019-02-21 14:00:18 -0500

    everything except for the sponsors quiz

---
+++ js/officersQuiz-config.js
@@ -0,0 +1,259 @@
+// Setup your quiz text and questions here
+
+// NOTE: pay attention to commas, IE struggles with those bad boys
+
+
+var quizJSON = {
+    "info": {
+        "name":    "FLBA National Officers!!",
+        "main":    "<p>Think you're smart enough to be on FBLA-Jeopardy? Find out with this FBLA national officers knowledge quiz!</p>",
+        "results": "<h5>Learn More</h5><p><a href='index.html'>Go back and take another quiz</a></p>",
+        "level1":  "FBLA-Jeopardy Ready",
+        "level2":  "FBLA-Jeopardy Contender",
+        "level3":  "FBLA-Jeopardy Amateur",
+        "level4":  "FBLA-Jeopardy Newb",
+        "level5":  "Stay in school, kid..." // no comma here
+    },
+    "questions": [
+ {
+   "q": "Who is the current FBLA national president?",
+   "a": [
+      {
+         "option": "Eu Wang",
+         "correct": true
+      },
+      {
+         "option": "Gail Hopkins",
+         "correct": false
+      },
+      {
+         "option": "Obi Tan",
+         "correct": false
+      },
+      {
+         "option": "Fiona Aster",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Good call.</span></p>",
+   "incorrect": "<p><span>Well, no.</span> Eu Wang</p>",
+   "select_any": ""
+},
+ {
+   "q": "Which of the following roles is in charge of ensuring meetings are conduted in accordance to proper procedure?",
+   "a": [
+      {
+         "option": "Parliamentarian",
+         "correct": true
+      },
+      {
+         "option": "Timekeeper",
+         "correct": false
+      },
+      {
+         "option": "Historian",
+         "correct": false
+      },
+      {
+         "option": "Proceduralist",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Good job.</span></p>",
+   "incorrect": "<p><span>Not quite.</span> Parliamentarian</p>",
+   "select_any": ""
+},
+ {
+   "q": "Which role is responsible for preparing the Local Chapter Annual Business Report?",
+   "a": [
+      {
+         "option": "Vice President",
+         "correct": true
+      },
+      {
+         "option": "President",
+         "correct": false
+      },
+      {
+         "option": "Head of Chapter",
+         "correct": false
+      },
+      {
+         "option": "Chapter Committee",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Excellent.</span></p>",
+   "incorrect": "<p><span>No.</span> Vice President</p>",
+   "select_any": ""
+},
+ {
+   "q": "Which of the following jobs does the FBLA National Secretary do?",
+   "a": [
+      {
+         "option": "Create a meeting agenda",
+         "correct": true
+      },
+      {
+         "option": "Keep track of the budget",
+         "correct": false
+      },
+      {
+         "option": "Maintain a history of the chapter",
+         "correct": false
+      },
+      {
+         "option": "Prepare a news article/release within one week of an activity",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Correct.</span></p>",
+   "incorrect": "<p><span>Incorrect.  </span> Create a meeting agenda</p>",
+   "select_any": ""
+},
+ {
+   "q": "Whose job is it to call special meetings as deemed necessary?",
+   "a": [
+      {
+         "option": "President",
+         "correct": true
+      },
+      {
+         "option": "Secretary",
+         "correct": false
+      },
+      {
+         "option": "Vice President",
+         "correct": false
+      },
+      {
+         "option": "Caller",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Good call.</span></p>",
+   "incorrect": "<p><span>No.  </span> President</p>",
+   "select_any": ""
+},
+ {
+   "q": "Who is in charge of handling the chapter's general correspondence?",
+   "a": [
+      {
+         "option": "Secretary",
+         "correct": true
+      },
+      {
+         "option": "Reporter",
+         "correct": false
+      },
+      {
+         "option": "Vice President",
+         "correct": false
+      },
+      {
+         "option": "President",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Good job.</span></p>",
+   "incorrect": "<p><span>Well, no.</span> Secretary</p>",
+   "select_any": ""
+},
+ {
+   "q": "Which current officer is in charge of keeping attendance and taking roll as National Secretary?",
+   "a": [
+      {
+         "option": "Keerti Soundappan",
+         "correct": true
+      },
+      {
+         "option": "Michael Zhao",
+         "correct": false
+      },
+      {
+         "option": "Madelyn Remington",
+         "correct": false
+      },
+      {
+         "option": "Ty Rickard",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Excellent.</span></p>",
+   "incorrect": "<p><span>Not quite.</span> Keerti Soundappan</p>",
+   "select_any": ""
+},
+ {
+   "q": "Which officer serves as an ex-officio non-voting member of every committee?",
+   "a": [
+      {
+         "option": "President",
+         "correct": true
+      },
+      {
+         "option": "Vice President",
+         "correct": false
+      },
+      {
+         "option": "Committee Chair",
+         "correct": false
+      },
+      {
+         "option": "Speaker",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Correct.</span></p>",
+   "incorrect": "<p><span>No.</span> President</p>",
+   "select_any": ""
+},
+ {
+   "q": "Which officer's role is to preside over regional meetings at the National Leadership Conference?",
+   "a": [
+      {
+         "option": "Vice President",
+         "correct": true
+      },
+      {
+         "option": "President",
+         "correct": false
+      },
+      {
+         "option": "Regional Manager",
+         "correct": false
+      },
+      {
+         "option": "Treasurer",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Good call.</span></p>",
+   "incorrect": "<p><span>Incorrect.  </span> Vice President</p>",
+   "select_any": ""
+},
+ {
+   "q": "Who does the secretary NOT need to send a copy of the meeting minutes and substantiating reports to?",
+   "a": [
+      {
+         "option": "Reporter",
+         "correct": true
+      },
+      {
+         "option": "President",
+         "correct": false
+      },
+      {
+         "option": "Chief Executive Officer",
+         "correct": false
+      },
+      {
+         "option": "Association President",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Good job.</span></p>",
+   "incorrect": "<p><span>No.  </span> Reporter</p>",
+   "select_any": ""
+}
+]
+};
---
+++ js/sponsorsQuiz-config.js
@@ -0,0 +1,258 @@
+// Setup your quiz text and questions here
+
+// NOTE: pay attention to commas, IE struggles with those bad boys
+
+
+var quizJSON = {
+    "info": {
+        "name":    "FLBA Sponsors!!",
+        "main":    "<p>Think you're smart enough to be on FBLA-Jeopardy? Find out with this FBLA sponsors quiz!</p>",
+        "results": "<h5>Learn More</h5><p><a href='index.html'>Go back and take another quiz</a></p>",
+        "level1":  "FBLA-Jeopardy Ready",
+        "level2":  "FBLA-Jeopardy Contender",
+        "level3":  "FBLA-Jeopardy Amateur",
+        "level4":  "FBLA-Jeopardy Newb",
+        "level5":  "Stay in school, kid..." // no comma here
+    },
+    "questions": [
+ {
+   "q": "Who is the founder of FLBA?",
+   "a": [
+      {
+         "option": "Hamden L. Forkner",
+         "correct": true
+      },
+      {
+         "option": "Robert Baden-Powell",
+         "correct": false
+      },
+      {
+         "option": "Theodore Roosevelt",
+         "correct": false
+      },
+      {
+         "option": "Juliette Low",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Good call.</span></p>",
+   "incorrect": "<p><span>Well, no.</span> Hamden L. Forkner</p>"
+},
+ {
+   "q": "FBLA was established in ____.",
+   "a": [
+      {
+         "option": "1940",
+         "correct": true
+      },
+      {
+         "option": "1878",
+         "correct": false
+      },
+      {
+         "option": "1972",
+         "correct": false
+      },
+      {
+         "option": "1969",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Good job.</span></p>",
+   "incorrect": "<p><span>Not quite.</span> 1940</p>",
+   "select_any": ""
+},
+ {
+   "q": "The first National Leadership Conference was held in  _____.",
+   "a": [
+      {
+         "option": "Chicago, Illinois in 1952",
+         "correct": true
+      },
+      {
+         "option": "Des Moines, Iowa in 1949",
+         "correct": false
+      },
+      {
+         "option": "New York, New York in 1967",
+         "correct": false
+      },
+      {
+         "option": "Nashville, Tennesee in 1971",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Excellent.</span></p>",
+   "incorrect": "<p><span>No.</span> Chicago, Illinois in 1952</p>",
+   "select_any": ""
+},
+ {
+   "q": "What foundation purchased 1.6 acres of land in the Center for Educational Associations, Reston, Virginia, as the site for a future FBLA-PBL National Center in 1981?",
+   "a": [
+      {
+         "option": "The Conrad N. Hilton Foundation",
+         "correct": true
+      },
+      {
+         "option": "The Carnegie Mellon Foundation",
+         "correct": false
+      },
+      {
+         "option": "The Ford Foundation",
+         "correct": false
+      },
+      {
+         "option": "The Rockefeller Foundation",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Correct.</span></p>",
+   "incorrect": "<p><span>Incorrect.  </span> The Conrad N. Hilton Foundation</p>",
+   "select_any": ""
+},
+ {
+   "q": "When was the last National Leadership Conference held in Washington, D.C.?",
+   "a": [
+      {
+         "option": "1996",
+         "correct": true
+      },
+      {
+         "option": "1972",
+         "correct": false
+      },
+      {
+         "option": "1985",
+         "correct": false
+      },
+      {
+         "option": "2002",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Good call.</span></p>",
+   "incorrect": "<p><span>No.  </span> 1996</p>",
+   "select_any": ""
+},
+ {
+   "q": "The first collegiate FBLA was chartered in _____.",
+   "a": [
+      {
+         "option": "Iowa",
+         "correct": true
+      },
+      {
+         "option": "Tennesee",
+         "correct": false
+      },
+      {
+         "option": "Ohio",
+         "correct": false
+      },
+      {
+         "option": "New York",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Good job.</span></p>",
+   "incorrect": "<p><span>Well, no.</span> Iowa</p>",
+   "select_any": ""
+},
+ {
+   "q": "Which state becomes the first FBLA state chapter? What year?",
+   "a": [
+      {
+         "option": "Iowa, 1947",
+         "correct": true
+      },
+      {
+         "option": "New York, 1948",
+         "correct": false
+      },
+      {
+         "option": "California, 1969",
+         "correct": false
+      },
+      {
+         "option": "Virginia, 1957",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Excellent.</span></p>",
+   "incorrect": "<p><span>Not quite.</span> Iowa, 1947</p>",
+   "select_any": ""
+},
+ {
+   "q": "When did FBLA-PBL is granted independent status as a nonprofit educational student association under Internal Revenue Code 501(c)(3)?",
+   "a": [
+      {
+         "option": "1969",
+         "correct": true
+      },
+      {
+         "option": "1971",
+         "correct": false
+      },
+      {
+         "option": "1942",
+         "correct": false
+      },
+      {
+         "option": "1959",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Correct.</span></p>",
+   "incorrect": "<p><span>No.</span> 1969</p>",
+   "select_any": ""
+},
+ {
+   "q": "The PBL in FBLA-PBL stands for _____ .",
+   "a": [
+      {
+         "option": "Phi Beta Lambda",
+         "correct": true
+      },
+      {
+         "option": "Pre-eminent Business Leaders",
+         "correct": false
+      },
+      {
+         "option": "Problem- Based Learning",
+         "correct": false
+      },
+      {
+         "option": "Power Business Logic",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Good call.</span></p>",
+   "incorrect": "<p><span>Incorrect.  </span> Phi Beta Lambda</p>",
+   "select_any": ""
+},
+ {
+   "q": "The FBLA-Middle Level division was started in ____ for students in grades 5-9.",
+   "a": [
+      {
+         "option": "1974",
+         "correct": true
+      },
+      {
+         "option": "1979",
+         "correct": false
+      },
+      {
+         "option": "1970",
+         "correct": false
+      },
+      {
+         "option": "1967",
+         "correct": false
+      }
+   ],
+   "correct": "<p><span>Good job.</span></p>",
+   "incorrect": "<p><span>No.  </span> 1974</p>",
+   "select_any": ""
+}
+    ]
+};
