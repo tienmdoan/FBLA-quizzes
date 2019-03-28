3e1fef7e1b4b923c88a76a453212b71b85db5534
tienmdoan <30280073+tienmdoan@users.noreply.github.com>
2019-03-27 21:24:31 -0400

    Update HistoryQuiz-config.js

--- www/js/HistoryQuiz-config.js
+++ www/js/HistoryQuiz-config.js
@@ -5,7 +5,7 @@
 
 var quizJSON = {
     "info": {
-        "name":    "FLBA History!!",
+        "name":    "FBLA History!!",
         "main":    "<p>Think you're smart enough to be on FBLA-Jeopardy? Find out with this FBLA history knowledge quiz!</p>",
         "results": "<h5>Learn More</h5><p><a href='index.html'>Go back and take another quiz</a></p>",
         "level1":  "FBLA-Jeopardy Ready",
